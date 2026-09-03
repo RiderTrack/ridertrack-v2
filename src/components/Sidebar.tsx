93caeea16f6e331f672a05a12e2c6f9dff354746
RiderTrack <indriver.pe@gmail.com>
2026-09-03 08:33:43 -0500

    Add files via upload

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -229,7 +229,7 @@ export const Sidebar: React.FC<SidebarProps> = ({
       <div className="flex lg:hidden items-center justify-between p-4 border-b border-slate-800">
         <div className="flex flex-col">
           <span className="font-bold text-white text-base">RiderTrack V2</span>
-          <span className="text-[10px] text-blue-400 font-bold font-mono tracking-wider">FASE 3.49</span>
+          <span className="text-[10px] text-blue-400 font-bold font-mono tracking-wider">FASE 3.50</span>
         </div>
         <button
           onClick={onCloseMobile}
